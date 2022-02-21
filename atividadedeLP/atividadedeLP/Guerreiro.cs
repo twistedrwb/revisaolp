b22e2e9be73fb6a2b345428cc235817b978a4cc7
twistedrwb <90223581+twistedrwb@users.noreply.github.com>
2022-02-21 09:24:55 -0300

    Add files via upload

---
+++ atividadedeLP/atividadedeLP/Guerreiro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadedeLP
+{
+    class Guerreiro
+    {
+        public string nome;
+        public int forca;
+        public int defesa;
+
+        public void ExibirNome()
+        {
+            Console.WriteLine("Nome: " + this.nome);
+        }
+        public void ExibirDefesa() 
+        {
+            Console.WriteLine("Defesa: " + this.defesa);
+        }
+        public void ExibirForca()
+        {
+            Console.WriteLine("Força: " + this.forca);
+        }
+        public bool Atacar(int DefesaInimigo)
+        {
+            if (this.forca > DefesaInimigo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+    }
+}
---
+++ atividadedeLP/atividadedeLP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadedeLP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guerreiro p1 = new Guerreiro();
+            p1.nome = "Guerreiro Toppers";
+            p1.forca = 100;
+            p1.defesa = 200;
+
+            Console.WriteLine("Nome: " + p1.nome);
+            Console.WriteLine("Força: " + p1.forca);
+            Console.WriteLine("Defesa: " + p1.defesa);
+
+            if (p1.Atacar(500))
+            {
+                Console.WriteLine("Resultado da luta: Vitória!");
+            }
+            else
+            {
+                Console.WriteLine("Resultado da luta: Perdeu!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
